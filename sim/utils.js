8af42ff945d3f1c2b21818bf249f2b6148af1854
havelll887 <aoyue1122@outlook.com>
2022-12-27 22:40:53 +0800

    some change

--- sim/utils.js
+++ sim/utils.js
@@ -1,22 +1,19 @@
 // Sim.js utilities
 Sim.Utils = {};
 
-Sim.Utils.orientationToQuaternion = function(vec)
-{
+Sim.Utils.orientationToQuaternion = function (vec) {
 	var axis = new THREE.Vector3(0, 1, 0);
 	var cross = new THREE.Vector3;
 	cross.cross(axis, vec);
-	if (!cross.length())
-	{
+	if (!cross.length()) {
 		axis.set(0, 0, 1);
 		cross.cross(vec, axis);
-		if (!cross.length())
-		{
+		if (!cross.length()) {
 			axis.set(1, 0, 0);
 			cross.cross(vec, axis);
 		}
 	}
-	
+
 	var angle = Math.acos(vec.dot(axis));
 
 	if (false) // angle > Math.PI / 2)
@@ -26,11 +23,11 @@ Sim.Utils.orientationToQuaternion = function(vec)
 		angle = Math.acos(vec.dot(axis));
 	}
 
-//		console.log("Cross: " + cross.x + ", " + cross.y + ", " + cross.z + "; Angle: " + angle)
-	
+	//		console.log("Cross: " + cross.x + ", " + cross.y + ", " + cross.z + "; Angle: " + angle)
+
 	var mat = new THREE.Matrix4().setRotationAxis(cross, angle);
 	var quaternion = new THREE.Quaternion();
 	mat.decompose(null, quaternion, null);
-	
+
 	return quaternion;
 }
